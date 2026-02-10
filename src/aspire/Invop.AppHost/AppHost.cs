6b89b45b4f079e9384af534af95a5802d3771f3b
Andrii Shandryk(Invop) <shandrykjj512@gmail.com>
2026-02-10 14:30:46 +0200

    Add project files.

---
+++ src/aspire/Invop.AppHost/AppHost.cs
@@ -0,0 +1,20 @@
+﻿using Aspire.Hosting;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var cache = builder.AddRedis("cache");
+
+var server = builder.AddProject<Projects.Invop_UrlShortener_Server>("server")
+    .WithReference(cache)
+    .WaitFor(cache)
+    .WithHttpHealthCheck("/health")
+    .WithExternalHttpEndpoints();
+var webfrontend = builder.AddViteApp("webfrontend", "../../url-shortener/Invop.UrlShortener.Frontend")
+    .WithNpm()
+    .WithReference(server)
+    .WaitFor(server);
+
+server.PublishWithContainerFiles(webfrontend, "wwwroot");
+
+builder.Build().Run();
+
