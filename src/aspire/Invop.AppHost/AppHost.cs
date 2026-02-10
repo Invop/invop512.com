a6d78a062976f702043cf585d32a3783dc473760
Andrii Shandryk(Invop) <shandrykjj512@gmail.com>
2026-02-10 14:52:07 +0200

    init invop512.com main front app

--- src/aspire/Invop.AppHost/AppHost.cs
+++ src/aspire/Invop.AppHost/AppHost.cs
@@ -1,20 +1,26 @@
-﻿using Aspire.Hosting;
+﻿#pragma warning disable IDE0059 // Unnecessary assignment of a value
+using Aspire.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
-var server = builder.AddProject<Projects.Invop_UrlShortener_Server>("server")
+var invop512 = builder.AddJavaScriptApp("invop512", "../../invop512.com")
+    .WithNpm()
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints();
+
+var urlShortenerServer = builder.AddProject<Projects.Invop_UrlShortener_Server>("urlShortenerServer")
     .WithReference(cache)
     .WaitFor(cache)
     .WithHttpHealthCheck("/health")
     .WithExternalHttpEndpoints();
-var webfrontend = builder.AddViteApp("webfrontend", "../../url-shortener/Invop.UrlShortener.Frontend")
+var urlShortenerWebFrontend = builder.AddViteApp("urlShortenerWebFrontend", "../../url-shortener/Invop.UrlShortener.Frontend")
     .WithNpm()
-    .WithReference(server)
-    .WaitFor(server);
+    .WithReference(urlShortenerServer)
+    .WaitFor(urlShortenerServer);
 
-server.PublishWithContainerFiles(webfrontend, "wwwroot");
+urlShortenerServer.PublishWithContainerFiles(urlShortenerWebFrontend, "wwwroot");
 
 builder.Build().Run();
 
