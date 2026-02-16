11390ac3c4ebf759416a79e15528233e2ae46760
Andrii Shandryk(Invop) <shandrykjj512@gmail.com>
2026-02-16 14:06:51 +0200

    init Token Range service to prevent token collisions

---
+++ src/url-shortener/Invop.UrlShortener.TokenRangeService/Program.cs
@@ -0,0 +1,15 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+builder.AddServiceDefaults();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapDefaultEndpoints();
+app.UseHttpsRedirection();
+
+app.Run();
